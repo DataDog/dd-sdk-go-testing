e49b7db15f3c31f5138b1bf7b47300d808473f90
Tony Redondo <tony.redondo@datadoghq.com>
2021-06-18 11:32:42 +0200

    Initial test suite.

--- init_test.go
+++ init_test.go
@@ -6,20 +6,105 @@
 package dd_sdk_go_testing
 
 import (
-	"os"
+	"fmt"
 	"testing"
+
+	"github.com/DataDog/dd-sdk-go-testing/internal/constants"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/ext"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/mocktracer"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
 
-func TestMain(m *testing.M) {
-	os.Exit(Run(m))
+func TestStatus(t *testing.T) {
+	mt := mocktracer.Start()
+	defer mt.Stop()
+
+	t.Run("pass", func(t *testing.T) {
+		ctx, finish := StartTest(t)
+		defer finish()
+
+		span, _ := tracer.SpanFromContext(ctx)
+		span.SetTag("k", "1")
+	})
+
+	t.Run("skip", func(t *testing.T) {
+		ctx, finish := StartTest(t)
+		defer finish()
+
+		span, _ := tracer.SpanFromContext(ctx)
+		span.SetTag("k", "2")
+		t.Skip("good reason")
+	})
+
+	spans := mt.FinishedSpans()
+	if len(spans) != 2 {
+		t.FailNow()
+	}
+
+	const suiteName string = "github.com/DataDog/dd-sdk-go-testing"
+	const framework string = "golang.org/pkg/testing"
+
+	s := spans[0]
+	assertEqual("test", s.OperationName())
+	assertEqual("TestStatus/pass", s.Tag(constants.TestName).(string))
+	assertEqual(suiteName, s.Tag(constants.TestSuite).(string))
+	assertEqual(fmt.Sprintf("%s.%s", suiteName, "TestStatus/pass"), s.Tag(ext.ResourceName).(string))
+	assertEqual(constants.TestStatusPass, s.Tag(constants.TestStatus).(string))
+	assertEqual(constants.SpanTypeTest, s.Tag(ext.SpanType).(string))
+	assertEqual(constants.SpanTypeTest, s.Tag(constants.SpanKind).(string))
+	assertEqual(constants.TestTypeTest, s.Tag(constants.TestType).(string))
+	assertEqual(framework, s.Tag(constants.TestFramework).(string))
+	assertEqual(constants.CIAppTestOrigin, s.Tag(constants.Origin).(string))
+	assertNotEmpty(s.Tag(constants.GitCommitAuthorDate).(string))
+	assertNotEmpty(s.Tag(constants.GitCommitAuthorEmail).(string))
+	assertNotEmpty(s.Tag(constants.GitCommitAuthorName).(string))
+	assertNotEmpty(s.Tag(constants.GitCommitCommitterDate).(string))
+	assertNotEmpty(s.Tag(constants.GitCommitCommitterEmail).(string))
+	assertNotEmpty(s.Tag(constants.GitCommitCommitterName).(string))
+	assertNotEmpty(s.Tag(constants.GitCommitMessage).(string))
+	assertNotEmpty(s.Tag(constants.GitCommitSHA).(string))
+	assertNotEmpty(s.Tag(constants.GitRepositoryURL).(string))
+	assertNotEmpty(s.Tag(constants.CIWorkspacePath).(string))
+	assertNotEmpty(s.Tag(constants.OSArchitecture).(string))
+	assertNotEmpty(s.Tag(constants.OSPlatform).(string))
+	assertNotEmpty(s.Tag(constants.OSVersion).(string))
+	fmt.Println(s)
+
+	s = spans[1]
+	assertEqual("test", s.OperationName())
+	assertEqual("TestStatus/skip", s.Tag(constants.TestName).(string))
+	assertEqual(suiteName, s.Tag(constants.TestSuite).(string))
+	assertEqual(fmt.Sprintf("%s.%s", suiteName, "TestStatus/skip"), s.Tag(ext.ResourceName).(string))
+	assertEqual(constants.TestStatusSkip, s.Tag(constants.TestStatus).(string))
+	assertEqual(constants.SpanTypeTest, s.Tag(ext.SpanType).(string))
+	assertEqual(constants.SpanTypeTest, s.Tag(constants.SpanKind).(string))
+	assertEqual(constants.TestTypeTest, s.Tag(constants.TestType).(string))
+	assertEqual(framework, s.Tag(constants.TestFramework).(string))
+	assertEqual(constants.CIAppTestOrigin, s.Tag(constants.Origin).(string))
+	assertNotEmpty(s.Tag(constants.GitCommitAuthorDate).(string))
+	assertNotEmpty(s.Tag(constants.GitCommitAuthorEmail).(string))
+	assertNotEmpty(s.Tag(constants.GitCommitAuthorName).(string))
+	assertNotEmpty(s.Tag(constants.GitCommitCommitterDate).(string))
+	assertNotEmpty(s.Tag(constants.GitCommitCommitterEmail).(string))
+	assertNotEmpty(s.Tag(constants.GitCommitCommitterName).(string))
+	assertNotEmpty(s.Tag(constants.GitCommitMessage).(string))
+	assertNotEmpty(s.Tag(constants.GitCommitSHA).(string))
+	assertNotEmpty(s.Tag(constants.GitRepositoryURL).(string))
+	assertNotEmpty(s.Tag(constants.CIWorkspacePath).(string))
+	assertNotEmpty(s.Tag(constants.OSArchitecture).(string))
+	assertNotEmpty(s.Tag(constants.OSPlatform).(string))
+	assertNotEmpty(s.Tag(constants.OSVersion).(string))
+	fmt.Println(s)
 }
 
-func TestRun(t *testing.T) {
-	ctx, finish := StartTest(t)
-	defer finish()
+func assertEqual(expected string, actual string) {
+	if expected != actual {
+		panic(fmt.Sprintf("Value expected: %s, Actual: %s", expected, actual))
+	}
+}
 
-	t.Run("sub-test", func(t *testing.T) {
-		_, finish := StartTestWithContext(ctx, t)
-		defer finish()
-	})
+func assertNotEmpty(actual string) {
+	if actual == "" {
+		panic("Value is empty")
+	}
 }
